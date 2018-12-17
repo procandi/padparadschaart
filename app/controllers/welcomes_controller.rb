582a1f5f8c35e4d7da1aa7c160fea21cc5218160
Ying-Hua Hsieh <procandi@gmail.com>
2018-12-17 18:23:19 +0800

    add picture to home page.

--- app/controllers/welcomes_controller.rb
+++ app/controllers/welcomes_controller.rb
@@ -1,6 +1,12 @@
 class WelcomesController < ApplicationController
   before_action :set_welcome, only: [:show, :edit, :update, :destroy]
 
+
+  # GET /welcomes
+  # GET /welcomes.json
+  def home
+  end
+
   # GET /welcomes
   # GET /welcomes.json
   def index
